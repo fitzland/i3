96733cd5e2bc91b19dab404bdcc5913e9e4b14cc
arcolinuxz <arcolinuxinfo@gmail.com>
2023-02-05 15:21:22 -0600

    adding current config

---
+++ bin/autostart.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+## Copyright (C) 2020-2022 Aditya Shakya <adi1090x@gmail.com>
+## Everyone is permitted to copy and distribute copies of this file under GNU-GPL3
+## Autostart Programs
+
+# Kill already running process
+_ps=(picom dunst lxpolkit mpd)
+for _prs in "${_ps[@]}"; do
+	if [[ `pidof ${_prs}` ]]; then
+		killall -9 ${_prs}
+	fi
+done
+
+# polkit agent
+if [[ ! `pidof lxpolkit` ]]; then
+	/usr/bin/lxpolkit &
+fi
+
+# num lock activated
+exec --no-startup-id numlockx on
+
+# Set Compose key
+setxkbmap -option compose:rctrl &
+
+# Fix cursor
+xsetroot -cursor_name left_ptr
+
+# Set display from arandr saved script
+sh ~/.screenlayout/monitor.sh &
+
+# Launch keybindings daemon
+#sxhkd -c $HOME/.config/i3/sxhkdrc &
+
+# Start dropbox
+exec dropbox start &
+
+# set wallpaper
+#exec --no-startup-id sleep 2 && nitrogen --restore
+
+# Launch colors
+~/.config/i3/bin/i3colors.sh
+
+# Launch notification daemon
+~/.config/i3/bin/i3dunst.sh
+
+# Launch polybar
+~/.config/i3/bin/i3bar.sh
+
+# Launch compositor
+~/.config/i3/bin/i3comp.sh
